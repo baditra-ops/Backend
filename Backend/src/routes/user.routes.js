6c509e5386396cb750662d6070e19c3f23b6f8ce
baditra-ops <baditrachoudhury@gmail.com>
2026-06-21 18:12:12 +0530

    Add user profile management features: implement change password, get current user, update account details, and update user avatar/cover image routes

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -2,7 +2,12 @@ import { Router } from "express";
 import { registerUser,
         loginUser,
         logoutUser,
-        refreshAccessToken
+        refreshAccessToken,
+        changeCurrentPassword,
+        getCurrentUser,
+        updateUserAvatar,
+        updateAccountDetails,
+        updateUserCoverImage
  } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -28,5 +33,11 @@ router.route("/register").post(
 
     router.route("/logout").post(verifyJWT,logoutUser)
     router.route("/refresh-token").post(refreshAccessToken)
+    router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+    router.route("/current-user").get(verifyJWT, getCurrentUser)
+    router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+    router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+    router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
 export default router
