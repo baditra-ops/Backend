57dc9ae883baf283cc9cc562e4412cb0e9afb644
baditra-ops <baditrachoudhury@gmail.com>
2026-06-23 11:35:30 +0530

    added tweet controller

--- Backend/src/controllers/tweet.controller.js
+++ Backend/src/controllers/tweet.controller.js
@@ -6,19 +6,116 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
+    const { content } = req.body
+
+    if (!content?.trim()) {
+        throw new ApiError(400, "Tweet content is required")
+    }
+
+    const tweet = await Tweet.create({
+        content,
+        owner: req.user._id
+    })
+
+    return res.status(201).json(
+        new ApiResponse(
+            201,
+            tweet,
+            "Tweet created successfully"
+        )
+    )
 })
+
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
+    const { userId } = req.params
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid user id")
+    }
+
+    const tweets = await Tweet.find({
+        owner: userId
+    }).sort({ createdAt: -1 })
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            tweets,
+            "Tweets fetched successfully"
+        )
+    )
 })
+
 
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
+    const { tweetId } = req.params
+    const { content } = req.body
+
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet id")
+    }
+
+    if (!content?.trim()) {
+        throw new ApiError(400, "Tweet content is required")
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+
+    if (!tweet) {
+        throw new ApiError(404, "Tweet not found")
+    }
+
+    if (tweet.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(
+            403,
+            "You can update only your own tweets"
+        )
+    }
+
+    tweet.content = content
+
+    await tweet.save()
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            tweet,
+            "Tweet updated successfully"
+        )
+    )
 })
 
+
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
+    const { tweetId } = req.params
+
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet id")
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+
+    if (!tweet) {
+        throw new ApiError(404, "Tweet not found")
+    }
+
+    if (tweet.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(
+            403,
+            "You can delete only your own tweets"
+        )
+    }
+
+    await Tweet.findByIdAndDelete(tweetId)
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            {},
+            "Tweet deleted successfully"
+        )
+    )
 })
 
 export {
