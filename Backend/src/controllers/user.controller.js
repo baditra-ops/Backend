44a698c373ef4e7794f3019cce0731c9493ff17a
baditra-ops <baditrachoudhury@gmail.com>
2026-06-30 13:13:33 +0530

    Add 'sameSite' attribute to cookie options in loginUser function

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -125,9 +125,10 @@ const loginUser = asyncHandler(async (req, res) =>{
     const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
 
     const options = {
-        httpOnly: true,
-        secure: true
-    }
+       httpOnly: true,
+       secure: true,
+       sameSite: "none"
+}
 
     return res
     .status(200)
